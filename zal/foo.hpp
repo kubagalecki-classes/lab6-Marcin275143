bedea847beae9e5e4ae40e686740d9f8120cff18
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-12-15 20:48:22 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -13,8 +13,8 @@ vector <char> foo(list <Human>& lista)
 {
 class Porownaj { public: char operator() (Human &osoba) { if (osoba.isMonster() != true) return 'y'; else return 'n'; osoba.birthday(); }; } porownanie;
         vector <char> Znaki (lista.size()); 
-    // char znak;
-    //  for_each(lista.rbegin(), lista.rend(), porownanie);
-        transform(lista.rbegin(), lista.rend(), Znaki.begin(), porownanie);
+    //  char znak;
+        for_each(lista.rbegin(), lista.rend(), porownanie);
+   //   transform(lista.rbegin(), lista.rend(), Znaki.begin(), porownanie);
         return Znaki;
 }
