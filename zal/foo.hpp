606381d79b98997620cc5c99d39a5ebdb831a34f
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-12-16 00:09:38 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,20 +1,22 @@
 //#pragma once
 
-
 #include <iostream>
 #include <algorithm>
 #include <list>
 #include <vector>
 #include "Human.hpp"
 
-using namespace std;
+class Porownaj { public: char operator() (Human& osoba) { if (osoba.isMonster() != true) return 'y'; else return 'n'; osoba.birthday();}; } porownanie;
+//  class Porownaj { public: void operator() (Human& osoba) { if (osoba.isMonster() != true) Znaki[nr_znaku] = 'y'; else Znaki[nr_znaku] = 'n'; osoba.birthday(); nr_znaku++; }; } porownanie; 
+//  auto transformacja[](Human czlowiek) { if (czlowiek.isMonster() != true) Znaki[nr_znaku] = 'y'; else Znaki[nr_znaku] = 'n'; czlowiek.birthday(); nr_znaku++; };
 
 vector <char> foo(list <Human>& lista)
 {
-    vector <char> Znaki (lista.size()); int  nr_znaku = 0;
-    class Porownaj { public: void operator() (Human& osoba) { if (osoba.isMonster() != true) Znaki[nr_znaku] = 'y'; else Znaki[nr_znaku] = 'n'; osoba.birthday(); nr_znaku++; }; } porownanie;
-//  class Porownaj { public: void operator() (Human& osoba) { if (osoba.isMonster() != true) return 'y'; else return 'n'; osoba.birthday();}; } porownanie;                           
-    for_each(lista.rbegin(), lista.rend(), porownanie);
-//  transform(lista.rbegin(), lista.rend(), Znaki[0], porownanie);
+    vector <char> Znaki(lista.size()); int  nr_znaku = 0;
+    
+//  for_each(lista.rbegin(), lista.rend(), transformacja);
+//  for_each(lista.rbegin(), lista.rend(), porownanie);
+
+    transform(lista.rbegin(), lista.rend(), Znaki.begin(), porownanie);
     return Znaki;
 }
