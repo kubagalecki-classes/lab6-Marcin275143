e77e6392e2ec5e2d92989728842a95d78ee124a3
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-12-16 09:34:59 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include "Human.hpp"
 
+using namespace std;
+
 class Porownaj { public: char operator() (Human& osoba) { if (osoba.isMonster() != true) return 'y'; else return 'n'; osoba.birthday();}; } porownanie;
 
 vector <char> foo(list <Human>& lista)
